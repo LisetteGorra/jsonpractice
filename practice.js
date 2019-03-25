8ec04b045e068fcc2773df4afec811869f245743
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-03-25 19:17:00 -0400

    Created button and get request on html

--- practice.js
+++ practice.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const app = express();
 
-app.use('/', function(req,res){
-   res.send('Hello CareerDevs');
-});
+// app.use('/', function(req,res){
+//   res.send('Hello CareerDevs');
+// });
 
+app.get("/api/get", (req,res)=>{
+    res.send("This is a responce from Homeabase");
+})
+
+app.post("/api/post", (req, res)=>{
+    res.send();
+})
 
 const port = process.env.PORT;
 const ip = process.env.IP;
 
 app.listen(port, ip, function(){
-    console.log('App listening on port: ${port}');
-    
+    console.log(`App listening on port: ${port}`);
+    console.log(`App is rining on ip: ${ip}`);
 })
